db710a2c882624e5dc59d0fcfa5211d32b4b0551
cubik707 <stdemidovez@gmail.com>
2023-11-19 16:26:02 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
+#include "Menu.h"
+#include <Windows.h>
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+
+	Menu menu;
+	menu.showContainerMenu();
 
 	return 0;
 }
